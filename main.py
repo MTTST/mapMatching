3aefecbd7ea8d7303b63a1a051d4d23b38b60e60
amartshah <amartshah@gmail.com>
2017-02-17 14:32:58 -0600

    amar > sonia

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv
 import sys
+from LatLon import LatLon
+import numpy as np
+
 
 link_file = open('probe_data_map_matching/Partition6467LinkData.csv', 'rb')
 probe_file = open('probe_data_map_matching/Partition6467ProbePoints.csv', 'rb')
@@ -32,7 +35,6 @@ for row in link_data:
     link['shapeInfo'] = r[14]
     link['curvatureInfo'] = r[15]
     link['slopeInfo'] = r[16]
-
     links.append(link)
 
 for row in probe_data:
@@ -56,13 +58,42 @@ for row in probe_data:
     probes[sample_id].append(d)
     counter += 1
 
+
 # print len(probes)
 # print len(links)
 for p in probes:
     current = probes[p]
     initial = current[0]
-    last = current[len(p)-1]
-    
+    last = current[len(current)-1]
+    initial_coord = LatLon(float(initial['lat']), float(initial['long']))
+    last_coord = LatLon(float(last['lat']), float(last['long']))
+    prob_vector = initial_coord - last_coord
+    angle_probe = prob_vector.heading
+    if angle_probe < 0:
+        angle_probe = angle_probe + 180
+    min_distance = 999999999999
+    min_index = 0
+
+    for i in xrange(len(links)):
+        l = links[i]
+        shapeInfo = l['shapeInfo'].replace('|', '/').split('/')
+        ref_coord = LatLon(float(shapeInfo[0]), float(shapeInfo[1]))
+        nref_coord = LatLon(float(shapeInfo[3]), float(shapeInfo[4]))
+        diff_vector = ref_coord - nref_coord
+        angle_link = diff_vector.heading
+        if angle_link < 0:
+            angle_link = angle_link + 180
+        print "vectors"
+        print diff_vector, prob_vector
+        print "angles"
+        print angle_link, angle_probe
+
+        distance = initial_coord.distance(ref_coord)
+        if distance <= min_distance:
+            min_distance = distance
+            min_index = i
+        sys.exit()
+
 
 
 
